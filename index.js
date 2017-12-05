6422976a48cfdbd3391b14bb6f6ee82a87dadff4
陈宇仑 <308813800@qq.com>
2017-12-05 14:30:03 +0800

    add:mongoose test

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 // Require the framework and instantiate it
-const fastify = require('fastify')();
+const fastify = require('fastify')({
+  logger: true,
+});
+
+fastify.register(require('fastify-mongoose'), {
+  uri: 'mongodb://127.0.0.1/test',
+}, (err) => {
+  if (err) throw err;
+  console.log('mongodb open');
+});
+
 fastify.register(require('./route/user'));
-// Declare a route
-fastify.get('/', async (req, res) => {
-  res.send({ hello: 'world' });
+
+fastify.ready((err) => {
+  if (err) throw err;
+  console.log('plugins are loaded');
+  for (const route of fastify) {
+    console.log(route);
+    /* will output:
+    {
+      '/route': {
+        get: {
+          method: String,
+          url: String,
+          schema: Object,
+          handler: Function,
+          Request: Function,
+          Reply: Function
+        }
+      }
+    }
+    */
+  }
 });
 
 // Run the server!
-fastify.listen(3000, (err) => {
+fastify.server.listen(3000, (err) => {
   if (err) {
     throw err;
   }
--- route/user.js
+++ route/user.js
@@ -1,4 +1,53 @@
+const mongoose = require('mongoose');
+
 async function user(fastify, options) {
-  fastify.get('/userInfo', async (request, reply) => ({ hello: 'world' }));
+  const animalSchema = new mongoose.Schema({ name: String, type: String });
+  const Animal = mongoose.model('Animal', animalSchema);
+
+  // Declare a route
+  fastify.get('/', async (req, res) => {
+    res.send({ hello: 'world' });
+  });
+
+  fastify.get('/search/:id', async (request, reply) => {
+    console.log(request.params.id);
+    const data = await Animal.findOne({ name: request.params.id });
+    return data;
+  });
+
+  const schemaBody = {
+    schema: {
+      body: {
+        type: 'object',
+        properties: {
+          someKey: { type: 'string' },
+          someOtherKey: { type: 'number' },
+        },
+      },
+    },
+  };
+  // fetch('/userInfo', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({}),
+  // });
+  fastify.post('/schemaBody', schemaBody, async (request, reply) => ({ hello: 'world' }));
+
+  const schemaResponse = {
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            hello: { type: 'string' },
+          },
+        },
+      },
+    },
+  };
+  // fetch('/schemaResponse');
+  fastify.get('/schemaResponse', schemaResponse, async (request, reply) => ({ hello: 'world', name: 'fastify' }));
 }
 module.exports = user;
